bb4da828b939ea2b9ae4776d15f929a144628ae0
kansuorhan <kansuorhan@gmail.com>
2022-10-01 14:00:30 +0300

    v1

---
+++ src/test/java/utilities/TestCaseBeforeAfterClass.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+import java.time.Duration;
+
+public class TestCaseBeforeAfterClass {
+
+    public  static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
