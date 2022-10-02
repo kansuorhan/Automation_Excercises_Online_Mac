e2c992c93350c947217c6b66fedcac18ad5fcae9
kansuorhan <kansuorhan@gmail.com>
2022-10-02 23:29:15 +0300

    v1

--- src/test/java/Automation_Excercises/TestCase09_Seacrh_Product.java
+++ src/test/java/Automation_Excercises/TestCase09_Seacrh_Product.java
@@ -1,2 +1,33 @@
-package Automation_Excercises;public class TestCase09_Seacrh_Product {
+package Automation_Excercises;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestCaseBeforeAfterClass;
+
+public class TestCase09_Seacrh_Product extends TestCaseBeforeAfterClass {
+
+    @Test
+    public void test() throws Exception {
+        /*
+         Launch browser
+        2. Navigate to url 'http://automationexercise.com'
+        3. Verify that home page is visible successfully
+        4. Click on 'Products' button
+        5. Verify user is navigated to ALL PRODUCTS page successfully
+        6. Enter product name in search input and click search button
+        7. Verify 'SEARCHED PRODUCTS' is visible
+        8. Verify all the products related to search are visible
+         */
+
+        //2
+        driver.get("http://automationexercise.com");
+
+        //3
+        WebElement category = driver.findElement(By.xpath("//h2[text()='Category']"));
+        Assert.assertTrue(category.isDisplayed());
+
+
+    }
 }
