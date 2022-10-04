e18165998eb5090c0bf0840dddd778cd84427889
kansuorhan <kansuorhan@gmail.com>
2022-10-04 15:21:30 +0300

    v1

--- src/test/java/Automation_Excercises/TestCase13_Verify_Product_Quantity_Cart.java
+++ src/test/java/Automation_Excercises/TestCase13_Verify_Product_Quantity_Cart.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestCaseBeforeAfterClass;
 
+import java.util.List;
+
 public class TestCase13_Verify_Product_Quantity_Cart extends TestCaseBeforeAfterClass {
     /*
     1. Launch browser
@@ -33,11 +35,12 @@ public class TestCase13_Verify_Product_Quantity_Cart extends TestCaseBeforeAfter
 
         //4
         WebElement product1000 = driver.findElement(By.xpath("(//h2[text()='Rs. 1000'])[5]"));
+        String product1000Price = driver.findElement(By.xpath("(//h2[text()='Rs. 1000'])[5]")).getText();
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("arguments[0].scrollIntoView(true);",product1000);
         Thread.sleep(2000);
 
-        //5
+        //5 - 6 - 7
         driver.findElement(By.xpath("(//a[@data-product-id='29'])[1]")).click();
         driver.findElement(By.xpath("//*[@class='btn btn-success close-modal btn-block']")).click();
         driver.findElement(By.xpath("(//a[@data-product-id='29'])[1]")).click();
@@ -45,7 +48,19 @@ public class TestCase13_Verify_Product_Quantity_Cart extends TestCaseBeforeAfter
         driver.findElement(By.xpath("(//a[@data-product-id='29'])[1]")).click();
         driver.findElement(By.xpath("//*[@class='btn btn-success close-modal btn-block']")).click();
         driver.findElement(By.xpath("(//a[@data-product-id='29'])[1]")).click();
+
+        //8
         driver.findElement(By.partialLinkText("View Cart")).click();
         Thread.sleep(2000);
+
+        //9
+        String productQuantity = driver.findElement(By.xpath("//button[@class='disabled']")).getText();
+        String cartPrice = driver.findElement(By.xpath("//*[@class='cart_price']")).getText();
+
+
+        Assert.assertEquals("4",productQuantity);
+
+        Assert.assertEquals(product1000Price,cartPrice);
+
     }
 }
