d3a2f54bae0bce2d2049c6f8ba67df9881bc2e52
kansuorhan <kansuorhan@gmail.com>
2022-10-12 11:42:10 +0300

    v1

--- src/test/java/Automation_Excercises/TestCase15_Place_Order_Register_Before_Checkout.java
+++ src/test/java/Automation_Excercises/TestCase15_Place_Order_Register_Before_Checkout.java
@@ -3,7 +3,9 @@ package Automation_Excercises;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestCaseBeforeAfterClass;
 
 public class TestCase15_Place_Order_Register_Before_Checkout extends TestCaseBeforeAfterClass {
@@ -38,6 +40,129 @@ public class TestCase15_Place_Order_Register_Before_Checkout extends TestCaseBef
 
         //4
         driver.findElement(By.xpath("//*[@class='fa fa-lock']")).click();
+
+        //5
+        driver.findElement(By.xpath("//input[@data-qa='signup-name']")).sendKeys("Ali Ata10");
+
+        driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys("aliatabak10@gmail.com");
+
+        driver.findElement(By.xpath("//button[@data-qa='signup-button']")).click();
+
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("12345");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("(//select[@class='form-control'])[1]")).sendKeys("5");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("(//select[@class='form-control'])[2]")).sendKeys("August");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("(//select[@class='form-control'])[3]")).sendKeys("1986");
+        Thread.sleep(1500);
+
+        WebElement newsletterCheck = driver.findElement(By.xpath("//input[@id='newsletter']"));
+        if (!newsletterCheck.isSelected()) {
+            newsletterCheck.click();
+        }
+
+
+        WebElement offerCheck = driver.findElement(By.xpath("//input[@id='optin']"));
+        if (!offerCheck.isSelected()) {
+            offerCheck.click();
+        }
+
+
+        driver.findElement(By.xpath("//input[@data-qa='first_name']")).sendKeys("Ali Ata5");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@data-qa='last_name']")).sendKeys("Bak");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@data-qa='address']")).sendKeys("Ahmet Mah. Aslan Sokak");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//select[@data-qa='country']")).sendKeys("United States");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@data-qa='state']")).sendKeys("Arizona");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@data-qa='city']")).sendKeys("Arizona State");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@data-qa='zipcode']")).sendKeys("75052");
+        Thread.sleep(1500);
+        driver.findElement(By.xpath("//input[@data-qa='mobile_number']")).sendKeys("+15123445656");
+        Thread.sleep(1500);
+
+        WebElement clickCreateAccountButton = driver.findElement(By.xpath("//button[@data-qa='create-account']"));
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].scrollIntoView(true);",clickCreateAccountButton);
+        Thread.sleep(2000);
+
+        clickCreateAccountButton.click();
+
+        //6
+        WebElement createText = driver.findElement(By.xpath("//h2[@data-qa='account-created']"));
+        Assert.assertTrue(createText.getText().contains("ACCOUNT CREATED!"));
+
+        driver.findElement(By.xpath("//a[@data-qa='continue-button']")).click();
+
+        //7
+        WebElement loggedIn = driver.findElement(By.xpath("//i[@class='fa fa-user']"));
+        Assert.assertTrue(loggedIn.isDisplayed());
+
+        //8
+        WebElement product1000 = driver.findElement(By.xpath("(//h2[text()='Rs. 1000'])[5]"));
+        String product1000Price = driver.findElement(By.xpath("(//h2[text()='Rs. 1000'])[5]")).getText();
+        jse.executeScript("arguments[0].scrollIntoView(true);",product1000);
+        Thread.sleep(2000);
+
+        //9
+        driver.findElement(By.xpath("(//a[@data-product-id='29'])[1]")).click();
+        driver.findElement(By.partialLinkText("View Cart")).click();
+
+        //10
+        WebElement checkoutPageButton = driver.findElement(By.xpath("//*[@class='btn btn-default check_out']"));
+        Thread.sleep(2000);
+        Assert.assertTrue(checkoutPageButton.isDisplayed());
+
+        //11
+        checkoutPageButton.click();
+
+        //12
+        WebElement devieryAddressPage = driver.findElement(By.xpath("//*[text()='Your delivery address']"));
+        Assert.assertTrue(devieryAddressPage.isDisplayed());
+
+        WebElement reviewOrderPage = driver.findElement(By.xpath("//*[text()='Review Your Order']"));
+        Assert.assertTrue(reviewOrderPage.isDisplayed());
+
+        //13
+        WebElement descriptionArea = driver.findElement(By.xpath("//*[@name='message']"));
+        Actions actions = new Actions(driver);
+        jse.executeScript("arguments[0].scrollIntoView(true);",descriptionArea);
+        Thread.sleep(1000);
+        actions.moveToElement(descriptionArea).sendKeys("Merhaba Yeni Dunya");
+
+        driver.findElement(By.xpath("//*[text()='Place Order']")).click();
+
+        //14
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@data-qa='name-on-card']")).sendKeys("Orhan");
+
+        driver.findElement(By.xpath("//*[@data-qa='card-number']")).sendKeys("1234567891011121");
+
+        driver.findElement(By.xpath("//*[@data-qa='cvc']")).sendKeys("311");
+
+        driver.findElement(By.xpath("//*[@data-qa='expiry-month']")).sendKeys("09");
+
+        driver.findElement(By.xpath("//*[@data-qa='expiry-year']")).sendKeys("2026");
+
+        //15
+        driver.findElement(By.xpath("//*[@id='submit']")).click();
+        Thread.sleep(5000);
+
+        //16
+        WebElement successText = driver.findElement(By.xpath("(//*[@class='alert-success alert'])[1]"));
+        Assert.assertTrue(successText.isDisplayed());
+        Thread.sleep(5000);
+
+        //17
+        driver.findElement(By.xpath("//*[text()=' Delete Account']")).click();
+
+        //18
+        driver.findElement(By.xpath("//*[text()='Delete']")).click();
 
 
     }
